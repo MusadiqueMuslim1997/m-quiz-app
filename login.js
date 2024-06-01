6df41e8ae687ac0e5b440eb1ceb573af5667a2a5
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-06-01 20:51:58 +0500

    add

--- login.js
+++ login.js
@@ -1,1 +1,11 @@
 
+
+
+
+function signup(){
+    window.location.assign('signup.html')
+}
+
+
+let signbtn = document.getElementById('signbtn');
+signbtn.addEventListener('click', signup);
--- signup.js
+++ signup.js
@@ -1,16 +1,26 @@
 
 
-import{auth, createUserWithEmailAndPassword} from './firebase.js';
+// import{auth, createUserWithEmailAndPassword} from './firebase.js';
 
 
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
   
-    const user = userCredential.user;
+//     const user = userCredential.user;
     
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
     
-  });
+//   });
+
+
+function sup(){
+    window.location.assign('index.html')
+}
+
+ let logbtn = document.getElementById('logbtn');
+ logbtn.addEventListener('click', sup)
+
+

a868da166c9e46a514c8c0e980cc4d228058b1d3
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-06-03 22:36:41 +0500

    add

---
+++ back.js
@@ -0,0 +1,8 @@
+
+
+
+function back(){
+    window.location.assign('card.html');
+}
+let backBtn = document.getElementById('back');
+backBtn.addEventListener('click', back)
---
+++ join.js
@@ -0,0 +1,64 @@
+
+let course_1 = document.getElementById('course1');
+let course_2 = document.getElementById('course2');
+let course_3 = document.getElementById('course3');
+let course_4 = document.getElementById('course4');
+let course_5 = document.getElementById('course5');
+let course_6 = document.getElementById('course6');
+let course_7 = document.getElementById('course7');
+let course_8 = document.getElementById('course8');
+let course_9 = document.getElementById('course9');
+
+function join1() {
+    window.location.assign('topic1.html')
+}
+course_1.addEventListener('click',join1);
+
+
+function join2() {
+    window.location.assign('topic2.html')
+}
+course_2.addEventListener('click',join2);
+
+
+function join3() {
+    window.location.assign('topic3.html')
+}
+course_3.addEventListener('click',join3);
+
+
+function join4() {
+    window.location.assign('topic4.html')
+}
+course_4.addEventListener('click',join4);
+
+
+function join5() {
+    window.location.assign('topic5.html')
+}
+course_5.addEventListener('click',join5);
+
+
+function join6() {
+    window.location.assign('topic6.html')
+}
+course_6.addEventListener('click',join6);
+
+
+function join7() {
+    window.location.assign('topic7.html')
+}
+course_7.addEventListener('click',join7);
+
+
+function join8() {
+    window.location.assign('topic8.html')
+}
+course_8.addEventListener('click',join8);
+
+
+function join9() {
+    window.location.assign('topic9.html')
+}
+course_9.addEventListener('click',join9);
+

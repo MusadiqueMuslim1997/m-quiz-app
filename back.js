0c1fb122ba3eccbfbf1213654b3b1040ab78d8ad
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-06-06 23:15:40 +0500

    add

--- back.js
+++ back.js
@@ -1,8 +1,12 @@
 
 
 
-function back(){
+function back() {
     window.location.assign('card.html');
 }
 let backBtn = document.getElementById('back');
 backBtn.addEventListener('click', back)
+
+function quiz() {
+window.location.assign('quiz.html')
+}

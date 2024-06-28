701b17b97c08f42403ca06b019ca8aa6f4b84086
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-06-28 15:35:42 +0500

    add

--- back.js
+++ back.js
@@ -24,11 +24,3 @@ function cssQuizKey() {
         }
         let quiz3 = document.getElementById('quiz3')
         quiz3.addEventListener('click',jsQuizKey)
-
-        let key = document.getElementById('key');
-        if(key.value == '123'){
-            window.location.assign('quiz.html')
-        }
-        else{
-            swal('Invalid key Please Enter 123')
-        }

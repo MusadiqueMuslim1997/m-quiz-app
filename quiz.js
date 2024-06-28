b89811985142ccb2de4d88bc0c2638d3165bdf50
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-06-28 21:17:06 +0500

    add

--- quiz.js
+++ quiz.js
@@ -9,83 +9,83 @@ var htmlQuiz = [
         ans: 'World Wide Web'
     },
 
-    // {
-    //     que: '2. What is the abbreviation of HTML?',
-    //     opt1: 'Hyper Text Markup Language',
-    //     opt2: 'Hyper Info Markup Language',
-    //     opt3: 'Hyper Tend Markup Language',
-    //     opt4: 'Hyper Test Markup Language',
-    //     ans: 'Hyper Text Markup Language'
-    // }
-    // ,
+    {
+        que: '2. What is the abbreviation of HTML?',
+        opt1: 'Hyper Text Markup Language',
+        opt2: 'Hyper Info Markup Language',
+        opt3: 'Hyper Tend Markup Language',
+        opt4: 'Hyper Test Markup Language',
+        ans: 'Hyper Text Markup Language'
+    }
+    ,
 
 
-    // {
-    //     que: '3. Choose the correct HTML element to define important text',
-    //     opt1: 'Strong',
-    //     opt2: 'I',
-    //     opt3: 'B',
-    //     opt4: 'Important',
-    //     ans: 'Important'
-    // },
-    // {
-    //     que: '4. In which of the following ways can we create strings in JavaScript?',
-    //     opt1: "'A string'",
-    //     opt2: '"A string"',
-    //     opt3: '`A string`',
-    //     opt4: 'All of the above',
-    //     ans: 'All of the above'
-    // },
+    {
+        que: '3. Choose the correct HTML element to define important text',
+        opt1: 'Strong',
+        opt2: 'I',
+        opt3: 'B',
+        opt4: 'Important',
+        ans: 'Important'
+    },
+    {
+        que: '4. In which of the following ways can we create strings in JavaScript?',
+        opt1: "'A string'",
+        opt2: '"A string"',
+        opt3: '`A string`',
+        opt4: 'All of the above',
+        ans: 'All of the above'
+    },
 
 
-    // {
-    //     que: '5. How to convert a string str to lowercase characters?',
-    //     opt1: 'str.lower()',
-    //     opt2: 'str.toLower()',
-    //     opt3: 'str.toLowerCase()',
-    //     opt4: 'None',
-    //     ans: 'str.toLowerCase()'
-    // },
-    // {
-    //     que: '6. var str = "I love cats!";  console.log(str.indexOf("cat"));',
-    //     opt1: '6',
-    //     opt2: '7',
-    //     opt3: '-1',
-    //     opt4: '8',
-    //     ans: '7'
-    // },
-    // {
-    //     que: '7. How can we extract "Pizza" from the string str="Pizza Lover"?',
-    //     opt1: 'str.slice(0, 4)',
-    //     opt2: 'str.slice(0, 5)',
-    //     opt3: 'str.slice(4, 0)',
-    //     opt4: 'str.slice(5, 0)',
-    //     ans: 'str.slice(0, 5)'
-    // },
-    // {
-    //     que: '8. What does str.charAt(1) return for the string str = "Hello"?',
-    //     opt1: '"H"',
-    //     opt2: '"e"',
-    //     opt3: '"l"',
-    //     opt4: 'undefined',
-    //     ans: '"e"'
-    // },
-    // {
-    //     que: '9. Suppose that str is equal to the string "Mango Apple  Orange". What will str.split() return?',
-    //     opt1: '["Mango", "Apple", "Orange"]',
-    //     opt2: '["Mango", "Apple", "", "Orange"]',
-    //     opt3: '["Mango Apple  Orange"]',
-    //     opt4: 'All of the above',
-    //     ans: '["Mango", "Apple", "Orange"]'
-    // },
-    // {
-    //     que: '10. The external JavaScript file must contain the <script> tag.',
-    //     opt1: 'True',
-    //     opt2: 'False',
-    //     opt3: 'All of the above',
-    //     opt4: 'None of the above',
-    //     ans: 'False'
-    // },
+    {
+        que: '5. How to convert a string str to lowercase characters?',
+        opt1: 'str.lower()',
+        opt2: 'str.toLower()',
+        opt3: 'str.toLowerCase()',
+        opt4: 'None',
+        ans: 'str.toLowerCase()'
+    },
+    {
+        que: '6. var str = "I love cats!";  console.log(str.indexOf("cat"));',
+        opt1: '6',
+        opt2: '7',
+        opt3: '-1',
+        opt4: '8',
+        ans: '7'
+    },
+    {
+        que: '7. How can we extract "Pizza" from the string str="Pizza Lover"?',
+        opt1: 'str.slice(0, 4)',
+        opt2: 'str.slice(0, 5)',
+        opt3: 'str.slice(4, 0)',
+        opt4: 'str.slice(5, 0)',
+        ans: 'str.slice(0, 5)'
+    },
+    {
+        que: '8. What does str.charAt(1) return for the string str = "Hello"?',
+        opt1: '"H"',
+        opt2: '"e"',
+        opt3: '"l"',
+        opt4: 'undefined',
+        ans: '"e"'
+    },
+    {
+        que: '9. Suppose that str is equal to the string "Mango Apple  Orange". What will str.split() return?',
+        opt1: '["Mango", "Apple", "Orange"]',
+        opt2: '["Mango", "Apple", "", "Orange"]',
+        opt3: '["Mango Apple  Orange"]',
+        opt4: 'All of the above',
+        ans: '["Mango", "Apple", "Orange"]'
+    },
+    {
+        que: '10. The external JavaScript file must contain the <script> tag.',
+        opt1: 'True',
+        opt2: 'False',
+        opt3: 'All of the above',
+        opt4: 'None of the above',
+        ans: 'False'
+    },
 ]
 
 let questionCount = 0;
@@ -110,8 +110,8 @@ let result = document.getElementById('result');
 let quizSec = document.getElementById('quizSec');
 let emoji = document.getElementById('emoji');
 let rank = document.getElementById('rank');
-
-
+let timeLeft = 300;
+let time = document.getElementById('time');
 
 
 
@@ -163,6 +163,22 @@ function next() {
     }
 }
 
+let startTimer = () => {
+    let interval = setInterval(() => {
+if(timeLeft <=0){
+    clearInterval(interval);
+    showResult();
+}
+else{
+    timeLeft--;
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    time.innerHTML = `${minutes} : ${seconds}`;
+}
+
+    },1000)
+}
+
 let showResult = () => {
 
     quizSec.style.display = 'none';
@@ -170,21 +186,21 @@ let showResult = () => {
     let percentage = Math.floor((score / htmlQuiz.length) * 100);
     per.innerHTML = percentage + '%';
     correctAns.innerHTML = "Correct Answer: " + score;
-    totalQue.innerHTML = "Total Question: " + htmlQuiz.length; 
-    if(percentage < 70){
+    totalQue.innerHTML = "Total Question: " + htmlQuiz.length;
+    if (percentage < 70) {
         announce.innerHTML = `Sorry! <br> 🥺 <br> You have Failed`;
         announce.style.color = 'red';
         per.style.color = 'red';
 
 
-       
+
     }
-    else{
+    else {
         announce.innerHTML = `Congratulation! <br>  😊 <br> You have Passed`;
-      
+
     }
 
 }
 
 rander()
-
+startTimer();
